8bbaf5c993cce291a315730a3199318dbfd99e36
Vicente <yepez@in.tum.de>
2019-10-08 15:17:16 +0200

    doc updated

--- R/package-tMAE.R
+++ R/package-tMAE.R
@@ -6,6 +6,10 @@
 #' @name tMAE
 #'
 #' @import data.table
+# #' @import tidyr
+#' @importFrom dplyr left_join
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 ggplot geom_point theme_bw scale_y_log10 scale_x_log10 labs scale_color_manual theme aes element_blank
 #' 
 #' @importFrom BiocGenerics estimateSizeFactors plotDispEsts
 #' 
@@ -16,8 +20,8 @@
 #'          estimateSizeFactorsForMatrix replaceOutliers
 #'          dispersions dispersions<- nbinomWaldTest results
 #' @importFrom GenomicRanges GRanges
-#' @importFrom GenomicScores populations
-#' @importFrom BiocGenerics score
+#' @importFrom GenomicScores populations seqnames
+#' @importFrom BiocGenerics score start pos
 # #' @importFrom MafDb.gnomAD.r2.1.hs37d5 MafDb.gnomAD.r2.1.hs37d5 # removed because of space
 # #' @importFrom MafDb.gnomAD.r2.1.GRCh38 MafDb.gnomAD.r2.1.GRCh38
 #' 
